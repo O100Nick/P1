e4d6f8bd907c7ee2707c512e0474dab4ad280e57
PedroMRGomes <119749315+PedroMRGomes@users.noreply.github.com>
2022-12-03 17:38:40 +0000

    Update dedos.c

--- dedos.c
+++ dedos.c
@@ -1,1 +1,16 @@
 
+int fun_inicio(char j1[100], char j2[100]){
+    printf("Jogo dos dedos - %s vs %s\n", j1, j2);
+    printf("%s 1: 1, 1\n", j1);
+    printf("\t\b%s 2: 1, 1\n", j2);
+}
+
+int main (int argc, char *argv[]){
+    char j1[100] = atof(argv[1]);
+    char j2[100] = atof(argv[2]);
+   /* printf("J1?");
+    scanf("%s", j1);
+    printf("J2?");
+    scanf("%s", j2);*/
+    fun_inicio(j1, j2);
+}
