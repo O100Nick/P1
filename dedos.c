f54fc5bfbc78803c94d971e3ae4a4944a9c7d683
PedroMRGomes <119749315+PedroMRGomes@users.noreply.github.com>
2022-12-08 18:52:35 +0000

    Update dedos.c

--- dedos.c
+++ dedos.c
@@ -36,6 +36,34 @@ int humano1(int *j1e, int *j1d, int *j2e, int *j2d, char *j1,  char *j2){
         return -1;
 }
 
+int humano2(int *j1e, int *j1d, int *j2e, int *j2d, char *j1,  char *j2){
+    char jd[100];
+    int veri;//,j1e,j1d,j2e,j2d;
+
+        printf("vez do humano 2:");
+        scanf("%s", jd);
+        if(strcmp(jd,"ee") == 0){
+            *j1e = *j1e + *j2e;
+            //printf("j1e: %d\n\n", *j1e);
+        }
+        if(strcmp(jd,"ed") == 0){
+            *j1d = *j2e + *j1d;
+            //printf("j1d: %d\n\n", *j1e);
+        }
+        if(strcmp(jd,"de") == 0){
+            *j1e = *j2d + *j1e;
+            //printf("j1d: %d\n\n", *j1d);
+        }
+        if(strcmp(jd,"dd") == 0){
+            *j1d = *j1d + *j2d;
+            //printf("j1d: %d\n\n", *j1d);
+        }
+        printf("%s 1:\t %d, %d\n", j1, *j1e, *j1d);
+        printf("%s 2:\t %d, %d\n", j2, *j2e, *j2d);
+
+        return -1;
+}
+
 /*int chico_esperto(){
 
     printf("\ndeu aqui\n");
@@ -67,7 +95,10 @@ MAIN(){
     int j2e = 1;
     int j2d = 1;
     
-    
+    if (!strcmp(argv[1], "humano") && !strcmp(argv[2], "humano")){
+        jogada_humano1(&j1e, &j1d, &j2e, &j2d, j1, j2);
+        jogada_humano2(&j1e, &j1d, &j2e, &j2d, j1, j2);
+    }
     if (!strcmp(argv[1], "humano"))
     {
         humano1(&j1e, &j1d, &j2e, &j2d, j1, j2);
@@ -86,7 +117,7 @@ MAIN(){
 
     if (!strcmp(argv[2], "humano"))
     {
-        humano2();
+       humano2(&j1e, &j1d, &j2e, &j2d, j1, j2);
 
     }
     else if (!strcmp(argv[2], "chico-esperto"))
